72ec1ec1c2fb950cca82e9d17d5674a21d1bad54
Sowad Mim <sowad1035@gmail.com>
2021-12-02 11:57:52 +0600

    feat: dahboard middleware

---
+++ pages/dashboard/_middleware.ts
@@ -0,0 +1,20 @@
+// @ts-nocheck
+import { getToken } from 'next-auth/jwt';
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const token = await getToken({
+    req: req,
+    secret: process.env.JWT_SECRET!,
+  });
+
+  if (!token) {
+    return NextResponse.redirect('/auth/login');
+  }
+
+  if (!token.user.isAdmin) {
+    return NextResponse.redirect('/');
+  }
+
+  return NextResponse.next();
+}
