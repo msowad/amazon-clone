ff7979aba0df00eac15c614e3545c8e989faf5c3
Sowad Mim <sowad.dev@gmail.com>
2022-10-22 21:30:37 +0600

    Revert "removed product api route and added product controller"
    This reverts commit f29203b651fe3a3e8c12560aed9dd9702d923418.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import AddToCart from "@/src/components/AddToCart";
-import { Layout } from "@/src/components/Layout";
-import PaginationLink from "@/src/components/PaginationLink";
-import { getProducts } from "@/src/server/controllers/products";
-import { PaginatedResponse } from "@/src/types/PaginatedResponse";
-import { Product } from "@/src/types/Product";
+import AddToCart from '@/src/components/AddToCart';
+import { Layout } from '@/src/components/Layout';
+import PaginationLink from '@/src/components/PaginationLink';
+import { PaginatedResponse } from '@/src/types/PaginatedResponse';
+import { Product } from '@/src/types/Product';
+import axios from '@/src/utils/axios';
 import {
   Card,
   CardActionArea,
@@ -14,11 +14,12 @@ import {
   Pagination,
   PaginationItem,
   Typography,
-} from "@mui/material";
-import { Box, styled } from "@mui/system";
-import type { GetServerSideProps, NextPage } from "next";
-import NextImage from "next/image";
-import NextLink from "next/link";
+} from '@mui/material';
+import { Box, styled } from '@mui/system';
+import type { GetServerSideProps, NextPage } from 'next';
+import NextImage from 'next/image';
+import NextLink from 'next/link';
+import React from 'react';
 
 const Home: NextPage<{ data: PaginatedResponse<Product[]> }> = ({ data }) => {
   return (
@@ -30,12 +31,12 @@ const Home: NextPage<{ data: PaginatedResponse<Product[]> }> = ({ data }) => {
               <NextLink href={`/products/${product.slug}`} passHref>
                 <CardActionArea>
                   <CardMedia title={product.name}>
-                    <div className="image-container">
+                    <div className='image-container'>
                       <NextImage
                         src={product.image}
                         alt={product.name}
-                        layout="fill"
-                        objectFit="contain"
+                        layout='fill'
+                        objectFit='contain'
                       />
                     </div>
                   </CardMedia>
@@ -43,28 +44,28 @@ const Home: NextPage<{ data: PaginatedResponse<Product[]> }> = ({ data }) => {
                 </CardActionArea>
               </NextLink>
               <CardActions>
-                <Typography className="price" variant="body2">
+                <Typography className='price' variant='body2'>
                   ${product.price}
                 </Typography>
                 <AddToCart
                   pid={product._id}
                   disabled={product.countInStock < 1}
-                  size="small"
-                  color="inherit"
+                  size='small'
+                  color='inherit'
                 />
               </CardActions>
             </Card>
           </Grid>
         ))}
       </StyledGrid>
-      <Box marginTop={5} display="flex" justifyContent="center">
+      <Box marginTop={5} display='flex' justifyContent='center'>
         <Pagination
           count={data.totalPages}
-          size="large"
+          size='large'
           renderItem={(item) => (
             <PaginationItem
               component={PaginationLink}
-              href={`/${item.page === 1 ? "" : `?page=${item.page}`}`}
+              href={`/${item.page === 1 ? '' : `?page=${item.page}`}`}
               {...item}
             />
           )}
@@ -79,24 +80,28 @@ export default Home;
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const page = Number(query.page) || 1;
   const limit = 20;
-  const field = "createdAt";
-  const sort = "desc";
-  const search = "";
+  const field = 'createdAt';
+  const sort = 'desc';
+  const search = '';
+
+  const { data } = await axios.get(
+    `/products?page=${page}&limit=${limit}&field=${field}&sort=${sort}&search=${search}`
+  );
 
   return {
     props: {
-      data: await getProducts(search, page, limit, field, sort),
+      data,
     },
   };
 };
 
 const StyledGrid = styled(Grid)(() => ({
-  ".image-container": {
-    width: "350px",
-    height: "300px",
-    position: "relative",
+  '.image-container': {
+    width: '350px',
+    height: '300px',
+    position: 'relative',
   },
-  ".price": {
-    marginRight: "10px",
+  '.price': {
+    marginRight: '10px',
   },
 }));
